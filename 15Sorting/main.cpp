964c4964d284a35cb025f46287a35f819c5d01e0
hackerman <sam@aflorithmic.ai>
2022-12-26 15:52:27 +0000

    Initial Commit

---
+++ 15Sorting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  15Sorting
+//
+//  Created by Hackerman on 26/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
